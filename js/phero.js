9e863edf22ff8ec25461a353687fd635fa9d9cdb
Arafa Akter <arafaakter924@gmail.com>
2023-08-31 21:09:04 +0600

    commit

--- js/phero.js
+++ js/phero.js
@@ -1,1 +1,20 @@
-console.log('found');
+const handelCategory = async() =>{
+    const response = await  fetch('https://openapi.programming-hero.com/api/videos/categories');
+
+    const data = await response.json();
+    // console.log(data.data);
+    const tabContainer = document.getElementById('tab-container');
+    data.data.forEach((element) =>{
+        const div = document.createElement("div");
+        div.innerHTML = `
+        <button class="btn btn-active">${element.category}</button>
+        `;
+        tabContainer.appendChild(div);
+    });
+
+
+    console.log(data.data);
+     
+};
+
+handelCategory()
