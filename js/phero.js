d874ccc4e2d021a839073d6d217ea2556d94de63
Arafa Akter <arafaakter924@gmail.com>
2023-09-01 19:07:36 +0600

    commit

--- js/phero.js
+++ js/phero.js
@@ -12,45 +12,45 @@ const handelCategory = async() =>{
         tabContainer.appendChild(div);
     });
     // console.log(data.data);
-     
+  
 };
+
 const handleLoadPage = async(elementId) =>{
    const response =await fetch(` https://openapi.programming-hero.com/api/videos/category/${elementId}`) 
    const data = await response.json();
-   console.log(data.data);
+  //  console.log(data.data);
    const cardContainer = document.getElementById('card-container');
    cardContainer.innerHTML ='';
    data.data.forEach((content) =>{
     const div = document.createElement('div');
    div.innerHTML =`
-   <div class="card  bg-base-100 shadow-xl">
+   <div class="card h-96 bg-base-100 shadow-xl">
    <figure >
-     <img class="w-50" src=${content?.thumbnail}
+     <img src=${content?.thumbnail}
        alt=""
      />
+     <p class="absolute right-5 bg-white mt-40 ">${content?.others.posted_date}</p>
    </figure>
    <div class="card-body ">
      <div class="card-footer flex justify-between ">
-       <div class="flex">
+       <div class="flex gap-5">
          <div>
-             <div class="w-14 rounded-full">
-               <img 
-                 src=${content.authors[0]?.profile_picture}
+               <img class="w-14 h-14 rounded-full" src=${content?.authors[0]?.profile_picture}
                />
              </div>
-         </div>
          <div>
-           <h6>${content.title}</h6>
+           <h6>${content?.title}</h6>
          </div>
        </div>
      </div>
      <div class="flex gap-7">
        <div>
-         <p>${content.authors[0].profile_name}</p>
-         <p>${content.others.views}</p>
+         <p>${content?.authors[0]?.profile_name}</p>
+         <p>${content?.others?.views}</p>
        </div>
        <div>
-       <i class="fa-solid fa-circle-check"></i>
+       <h3>${content.authors[0].verified}</h3>
+       
        </div>
      </div>
    </div>
@@ -62,5 +62,71 @@ const handleLoadPage = async(elementId) =>{
 
 };
 
+// sort by view
+ const sortFunction =async ()=>{
+  // console.log('clicked');
+  const response = await  fetch('https://openapi.programming-hero.com/api/videos/category/1000');
+
+  const data = await response.json();
+  // console.log(data.data);
+  data.data.forEach((views) =>{
+    const viewsAmount = views.others.posted_date;
+    console.log(viewsAmount);
+
+  })
+  
+ }
+
+// blog button
+function myBlog() {
+  console.log('clicked');
+  window.location.href='../blog.html';
+}
+//go home button
+function home(){
+  console.log('gg');
+  window.location.href='index.html';
+}
+
+
+
+
+// function hoursAndMinutes (TotalSecond){
+//   const totalMinutes = Math.floor(TotalSecond/60);
+//   const seconds = TotalSecond%60;
+//   const hours =Math.floor(totalMinutes/60);
+//   const minutes = totalMinutes%60;
+//   return{h:hours,m:minutes,s:seconds};
+
+// }
+
+
 handelCategory()
 handleLoadPage ('1000')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* <i class="fa-solid fa-badge-check"></i> */
