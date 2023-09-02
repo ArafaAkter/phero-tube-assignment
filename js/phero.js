62a7ff8a5f6ef9f85710765e8bb7443756e0cdf1
Arafa Akter <arafaakter924@gmail.com>
2023-09-02 17:59:54 +0600

    updated

--- js/phero.js
+++ js/phero.js
@@ -7,20 +7,29 @@ const handelCategory = async() =>{
     data.data.forEach((element) =>{
         const div = document.createElement("div");
         div.innerHTML = `
-        <button onclick ="handleLoadPage('${element.category_id?element.category_id : 'No data' }')" class="btn btn-active">${element.category}</button>
+        <button onclick ="handleLoadPage('${element.category_id}')" class="btn btn-active">${element.category}</button>
         `;
         tabContainer.appendChild(div);
     });
-    // console.log(data.data);
+    // console.log(data.data.length);
   
 };
 
 const handleLoadPage = async(elementId) =>{
    const response =await fetch(` https://openapi.programming-hero.com/api/videos/category/${elementId}`) 
    const data = await response.json();
-  //  console.log(data.data);
+  //  console.log(data.data.length);
    const cardContainer = document.getElementById('card-container');
    cardContainer.innerHTML ='';
+
+    //hidden div
+    console.log(data.data);
+    const hiddenDiv = document.getElementById('hidden-div');
+    if(data.data.length == 0){
+      hiddenDiv.classList.remove('hidden');
+    }else{
+      hiddenDiv.classList.add('hidden')
+    }
 
    data.data.forEach((content) =>{
     const div = document.createElement('div');
@@ -31,15 +40,13 @@ const handleLoadPage = async(elementId) =>{
     const hours =Math.floor(totalMinutes/60);
     const minutes = totalMinutes%60;
 
-
-
    div.innerHTML =`
-   <div class="card h-96 bg-base-100 shadow-xl">
-   <figure >
-     <img src=${content?.thumbnail}
+   <div class="card relative w-auto h-80 bg-base-100 shadow-xl">
+   <figure class="h-50">
+     <img  src=${content?.thumbnail}
        alt=""
      />
-     <p class="absolute right-5 bg-white mt-40 ">${content?.others.posted_date?`${hours} hrs ${minutes} min ago`:''}</p>
+     <p class="absolute right-5 -top-12 bg-white mt-40 ">${content?.others.posted_date?`${hours} hrs ${minutes} min ago`:''}</p>
    </figure>
    <div class="card-body ">
      <div class="card-footer flex justify-between ">
